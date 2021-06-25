906d1a89a6a50543e575f9ebb02764c8074fae19
Anand J <anandj@cs.ubc.ca>
2021-06-24 23:51:31 -0400

    test code

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ pip install keras_preprocessing --no-deps
 
 bazel build -j 20 --copt=-g --config=opt --config=noaws --config=nogcp --config=nohdfs --config=nonccl --verbose_failures //tensorflow/tools/pip_package:build_pip_package
 
-./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/anand/tensorflow_pkg
 
-pip install -U --no-deps /tmp/tensorflow_pkg/tensorflow-2.4.0-cp38-cp38-linux_x86_64.whl
+pip install -U --no-deps /tmp/anand/tensorflow_pkg/tensorflow-2.4.0-cp36-cp36m-linux_x86_64.whl
