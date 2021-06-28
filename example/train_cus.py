2d6b012d575ddcc1bbc1805f06e5b8ccf93e9e94
Anand J <anandj@cs.toronto.edu>
2021-06-28 13:35:49 -0400

    Keep tf.Variable as fp32

--- example/train_cus.py
+++ example/train_cus.py
@@ -2,10 +2,10 @@ import numpy as np
 import tensorflow as tf
 import os
 
-os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '1'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
-#os.environ['TF_DUMP_GRAPH_PREFIX'] = '/tmp/tf_graphs/'
-#os.environ['XLA_FLAGS'] = "--xla_dump_hlo_as_text --xla_dump_to=/home/chenhao/projects/xla_dump/mlp"
+os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_DUMP_GRAPH_PREFIX'] = '/tmp/tf_graphs/'
+os.environ['XLA_FLAGS'] = "--xla_dump_hlo_as_text --xla_dump_to=/tmp/tf2xla/xla_dump/float"
 
 class Model:
     def __init__(self, lr, num_class, dtype):
@@ -15,15 +15,16 @@ class Model:
 
         self.W = self._get_weight([5, 5], "weight")
         self.b = self._get_weight([5], "bias")
-        self.weights = [self.W, self.b]
+        self.weights = [self.W]
 
     def _get_weight(self, shape, name):
-        return tf.Variable(tf.cast(np.ones(shape), dtype=self.dtype), name=name, trainable=True, dtype=self.dtype)
+        return tf.Variable(np.ones(shape, dtype=np.float32), name=name, trainable=True, dtype=tf.float32)
 
-    @tf.function(experimental_compile=True)
     def forward(self, x):
         x = tf.cast(x, dtype=self.dtype)
-        out = (tf.matmul(x, self.W) + self.b)
+        W = tf.cast(self.W, dtype=self.dtype)
+        #b = tf.cast(self.b, dtype=self.dtype)
+        out = tf.matmul(x, W)
         out = tf.cast(out, dtype=tf.float32)
         return out
 
@@ -39,21 +40,23 @@ num_epochs = 5 #@param {type: "number"}
 dataset = "mnist"
 num_class = 10
 
-model = Model(learning_rate, num_class, tf.float64)
-#model = Model(learning_rate, num_class, tf.cus)
+#model = Model(learning_rate, num_class, tf.float64)
+model = Model(learning_rate, num_class, tf.cus)
 
+@tf.function(experimental_compile=True)
 def train_step(model, x, y):
     with tf.GradientTape() as tape:
         pred = model.forward(x)
+        loss = model.loss(y, pred)
     grads = tape.gradient(pred, model.weights)
+    model.update(grads)
     return grads
 
 with open('example/data.npy', 'rb') as f:
-    x = np.load(f)
-    y = np.load(f)
+    x = np.load(f).astype('float32')
+    y = np.load(f).astype('float32')
 
-grad = train_step(model, x, y)
-print("W:", model.W)
-print("B:", model.b)
-print("dW:", grad[0])
-print("db:", grad[1]) 
+for i in range(0, 1000):
+    grad = train_step(model, x, y)
+    print("W:", model.W)
+    print("dW:", grad[0])
